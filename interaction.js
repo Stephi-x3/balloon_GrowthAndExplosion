ac9333c01ea39db13d2e060619511d3576f49e10
Stephi <139639918+Stephi-x3@users.noreply.github.com>
2023-09-12 17:21:14 +0200

    growth,shrink,explode balloon

--- interaction.js
+++ interaction.js
@@ -1,11 +1,36 @@
 document.addEventListener("keydown", KeyboardEvent => {
-    if(KeyboardEvent.code=='ArrowUp'){
+    if (KeyboardEvent.code=='ArrowUp' || KeyboardEvent.code=='ArrowDown') {
         const balloon = document.getElementById('gameObject');
         const currentSize = parseFloat(getComputedStyle(balloon).getPropertyValue('font-size'));
-        const newSize = currentSize + 25;
-        balloon.style.fontSize = newSize+'px';
-    }else if(KeyboardEvent.code=='ArrowDown'){
-        console.log('hello');
+        let newSize = 0;
+
+        if(KeyboardEvent.code=='ArrowUp'){
+            newSize = currentSize + 25;
+            
+            if (newSize>=400) {
+               balloon.textContent = '💥';
+               document.getElementById('gameOver').style.display = 'block';
+            }
+            balloon.style.fontSize = newSize+'px';
+
+        }else if(KeyboardEvent.code=='ArrowDown'){
+            newSize = currentSize - 25;
+            
+            if (newSize<50) {
+                return;
+             }
+            balloon.style.fontSize = newSize+'px';
+        }
     }
-});
     
+    
+/*
+    // CSS-Klasse "animated-text" hinzufügen, um die Animation zu aktivieren
+    balloon.classList.add("animated-text");
+
+    // Nach Abschluss der Animation CSS-Klasse entfernen
+    balloon.addEventListener("animationend", function() {
+    balloon.classList.remove("animated-text");
+    });*/
+});
+
